1f6ee0b4d1931b49285e67929d3b53c5a2d3f6fd
David Francisco <dfrancisco2677@gmail.com>
2022-05-16 12:00:47 -0700

    Sign up form works properly and adds row to supabase table.

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,6 +1,6 @@
 // Enter Supabase Information
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://edanuxzgkcleuccdeakc.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVkYW51eHpna2NsZXVjY2RlYWtjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTIzNjg3OTMsImV4cCI6MTk2Nzk0NDc5M30.HH23nUpy7jCA4AgRRgpIk8M-fwc5zqvBLuE1Muk_lps';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
@@ -8,7 +8,10 @@ export function getUser() {
     return client.auth.session() && client.auth.session().user;
 }
 
-export async function signupUser(email, password) {}
+export async function signupUser(email, password) {
+    const response = await client.auth.signUp({ email, password });
+    return response.user;
+}
 
 export async function signInUser(email, password) {}
 
